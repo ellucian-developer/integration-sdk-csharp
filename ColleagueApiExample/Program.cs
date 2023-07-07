0aa0df8e346f429622271b867d4fbfa6f792e680
Neal Webb <nwebb@ferrilli.com>
2023-07-07 19:21:15 -0400

    Add sample project using the client to access the Colleague API

---
+++ ColleagueApiExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using Ellucian.Ethos.Integration.Client;
+using Ellucian.Ethos.Integration.Client.Filter.Extensions;
+using Ellucian.Ethos.Integration.Client.Proxy.Filter;
+using Newtonsoft.Json.Linq;
+
+var proxyClient =
+    new EthosClientBuilder(
+        colleagueApiUrl: "",
+        colleagueApiUsername: "",
+        colleagueApiPassword: "")
+        .BuildColleagueWebApiProxyclient();
+
+var academicPeriod =
+    await proxyClient.GetAsJObjectByIdAsync("academic-periods", "a4b5fddc-fa2f-4e94-82e9-cbe219a5029b");
+
+Console.WriteLine(academicPeriod.ToString());
+
+var queryClient =
+    new EthosClientBuilder(
+        colleagueApiUrl: "",
+        colleagueApiUsername: "",
+        colleagueApiPassword: "")
+    .BuildColleagueWebApiFilterQueryClient();
+
+var filter =
+    new CriteriaFilter()
+        .WithSimpleCriteria("startOn", ("$gte", "2020-01-01"));
+
+var responses =
+    await queryClient.GetPagesWithCriteriaFilterAsync("academic-periods", filter);
+
+foreach (var response in responses)
+{
+    var acadPeriods = JArray.Parse(response.Content);
+
+    Console.WriteLine(acadPeriods.ToString());
+}
